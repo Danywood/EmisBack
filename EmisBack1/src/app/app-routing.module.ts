17496bc6373e696ff9171fae2b44ec4b7a236fa6
Danywood <danywood11@gmail.com>
2024-05-21 01:27:19 +0200

    Insert app routing configuration file

---
+++ EmisBack1/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+/*
+* File di configurazione per il routing delle pagine
+*/
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+//import { HomeComponent } from './home/home.component';
+//import { MenuComponent } from './menu/menu.component';
+//import { AboutComponent } from './about/about.component';
+//import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  /*{ path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }*/
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
