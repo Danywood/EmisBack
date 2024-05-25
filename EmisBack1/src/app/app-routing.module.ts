db24216c030f7248d101c2512c99907af8edd72a
Danywood <danywood11@gmail.com>
2024-05-25 18:41:55 +0200

    Add navigation-bar component

--- EmisBack1/src/app/app-routing.module.ts
+++ EmisBack1/src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component'; //parte in alto della pagina , conterrà navigation bar
 import { MiddleComponent } from './middle/middle.component'; // parte centrale della pagina , conterrà 
 import { FooterComponent } from './footer/footer.component'; // parte in fondo della pagina , conterrà 
-//import { ContactComponent } from './contact/contact.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: 'middle', component: MiddleComponent },
   { path: 'footer', component: FooterComponent },
+  { path: 'navigation-bar', component: NavigationBarComponent },
 ];
 
 @NgModule({
