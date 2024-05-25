45ed465febd0edf5d517fb9657864a1a01683522
Danywood <danywood11@gmail.com>
2024-05-25 18:36:00 +0200

    Add component to routing and app.component

--- EmisBack1/src/app/middle/middle.component.spec.ts
+++ EmisBack1/src/app/middle/middle.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenuComponent } from './menu.component';
+import { MiddleComponent } from './middle.component';
 
-describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+describe('MiddleComponent', () => {
+  let component: MiddleComponent;
+  let fixture: ComponentFixture<MiddleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuComponent]
+      imports: [MiddleComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(MenuComponent);
+    fixture = TestBed.createComponent(MiddleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
