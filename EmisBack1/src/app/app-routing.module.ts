99a5956c21f345c0c10eacfb8aac8a69c572d765
Danywood <danywood11@gmail.com>
2024-05-25 18:48:30 +0200

    Add title and navigation component

--- EmisBack1/src/app/app-routing.module.ts
+++ EmisBack1/src/app/app-routing.module.ts
@@ -7,12 +7,16 @@ import { HeaderComponent } from './header/header.component'; //parte in alto del
 import { MiddleComponent } from './middle/middle.component'; // parte centrale della pagina , conterrà 
 import { FooterComponent } from './footer/footer.component'; // parte in fondo della pagina , conterrà 
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { NavigationButtonComponent } from './navigation-button/navigation-button.component';
+import { TitleComponent } from './title/title.component';
 
 const routes: Routes = [
   { path: 'header', component: HeaderComponent },
   { path: 'middle', component: MiddleComponent },
   { path: 'footer', component: FooterComponent },
   { path: 'navigation-bar', component: NavigationBarComponent },
+  { path: 'navigation-button', component: NavigationButtonComponent },
+  { path: 'title', component: TitleComponent },
 ];
 
 @NgModule({
