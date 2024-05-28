292afe7ea2eee11734e4989da19005ed26d3863e
Giuseppe <giuseppe.passarello@hotmail.com>
2024-05-28 17:17:58 +0200

    aggiunta immagine sfondo + body

--- EmisBack1/src/app/header/header.component.ts
+++ EmisBack1/src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavigationBarComponent } from "../navigation-bar/navigation-bar.component";
+import { TitleComponent } from "../common/title/title.component";
 
 @Component({
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
-    imports: [NavigationBarComponent]
+    imports: [NavigationBarComponent, TitleComponent]
 })
 export class HeaderComponent {
 
