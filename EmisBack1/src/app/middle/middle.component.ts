044e85436a805c4e91171fc903ee369434dbb3ec
Giuseppe <giuseppe.passarello@hotmail.com>
2024-06-02 22:29:55 +0200

    creazione nuovi componenti per le card centrali + migliorato css

--- EmisBack1/src/app/middle/middle.component.ts
+++ EmisBack1/src/app/middle/middle.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ItemListComponent } from "./item-list/item-list.component";
 
 @Component({
-  selector: 'app-middle',
-  standalone: true,
-  imports: [],
-  templateUrl: './middle.component.html',
-  styleUrl: './middle.component.css'
+    selector: 'app-middle',
+    standalone: true,
+    templateUrl: './middle.component.html',
+    styleUrl: './middle.component.css',
+    imports: [ItemListComponent]
 })
 export class MiddleComponent {
 
