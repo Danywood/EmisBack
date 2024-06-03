55f3e7ee35de6347939843e6c11affce6a86d6ad
Giuseppe <giuseppe.passarello@hotmail.com>
2024-06-03 20:04:08 +0200

    aggiustati componenti , inseriti direttive sul componenti a scomparsa

--- EmisBack1/src/app/footer/footer.component.ts
+++ EmisBack1/src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+   onSubmit() {
+    console.log("click");
+  } 
 
 }
